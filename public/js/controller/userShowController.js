eb9c5bf46664cc9e2091c3727aca465a004b94ce
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-14 22:18:57 +0200

    taglie table

--- public/js/controller/userShowController.js
+++ public/js/controller/userShowController.js
@@ -5,7 +5,8 @@ angular.module('boggiApp')
 			 $scope.birthday=1;
 
 		   $scope.done = false;
-		   $scope.info = [];
+			 $scope.info=[];
+		   $scope.infos = [];
 		   $scope.parseDate = function(date){
 		       var ms = parseInt(date.slice(6, 19), 10);
 		       var parsedDate = new Date(ms);
@@ -24,11 +25,34 @@ angular.module('boggiApp')
                 url: '/api/v1/user/' + $stateParams.userEmail,
             }).then(function(response) {
                 console.log(response);
+								$scope.size=[];
                 $scope.done = true;
+								$scope.OrderDate=new Date(0);
+
                 $scope.data = response.data;
 								if (response.data.DemandwareCustomer.Birthday==$scope.date){
 									$scope.birthday=0;
 								}
+								for(var order in response.data.Orders){
+
+                    for(var item in response.data.Orders[order].OrderBoggiItems){
+                        var trovato = false;
+
+                        for(var obj in $scope.infos){
+                            if($scope.infos[obj].name==response.data.Orders[order].OrderBoggiItems[item].GroupDescription){
+															if ($scope.parseDate(response.data.Orders[order].DataOrdine)>=$scope.OrderDate){
+	                                trovato = true;
+	                            }
+                        		}
+                    		}
+												if(!trovato){
+													$scope.OrderDate=$scope.parseDate(response.data.Orders[order].DataOrdine);
+													$scope.infos.push({date:$scope.OrderDate, name:response.data.Orders[order].OrderBoggiItems[item].GroupDescription, size: response.data.Orders[order].OrderBoggiItems[item].Taglia});
+												}
+										}
+                }
+								console.log($scope.infos);
+
                 for(var order in response.data.Orders){
                     for(var item in response.data.Orders[order].OrderBoggiItems){
                         var found = false;
