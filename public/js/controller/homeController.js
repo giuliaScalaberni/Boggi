b0d273542d77605070858f2e4e5171ae4c46ef8c
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-14 18:48:54 +0200

    better index

--- public/js/controller/homeController.js
+++ public/js/controller/homeController.js
@@ -1,4 +1,11 @@
 angular.module('boggiApp')
 	.controller('homeController', function($scope){
-		
+
+		$scope.check=function(){
+			if($scope.email==""){
+				alert('error');
+				 
+			}
+
+		};
 	});
