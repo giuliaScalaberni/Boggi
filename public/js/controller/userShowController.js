fc586859085b854ba707423269ba618f12de6fae
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-14 22:51:59 +0200

    birthday month

--- public/js/controller/userShowController.js
+++ public/js/controller/userShowController.js
@@ -3,7 +3,7 @@ angular.module('boggiApp')
 		.controller('userShowController', function($scope, $stateParams, $http){
 			$scope.date= new Date();
 			 $scope.birthday=1;
-
+			 $scope.gift=1;
 		   $scope.done = false;
 			 $scope.info=[];
 		   $scope.infos = [];
@@ -20,24 +20,24 @@ angular.module('boggiApp')
 		       return str;
 		   }
 
+
 		   $http({
                 method: 'GET',
                 url: '/api/v1/user/' + $stateParams.userEmail,
             }).then(function(response) {
-                console.log(response);
 								$scope.size=[];
                 $scope.done = true;
 								$scope.OrderDate=new Date(0);
-
                 $scope.data = response.data;
-								if (response.data.DemandwareCustomer.Birthday==$scope.date){
+								console.log((new Date(response.data.DemandwareCustomer.Birthday)).getMonth());
+								if ((new Date(response.data.DemandwareCustomer.Birthday)).getMonth()==$scope.date.getMonth()){
 									$scope.birthday=0;
 								}
 								for(var order in response.data.Orders){
 
                     for(var item in response.data.Orders[order].OrderBoggiItems){
+											if (!item.OrdineRegalo){
                         var trovato = false;
-
                         for(var obj in $scope.infos){
                             if($scope.infos[obj].name==response.data.Orders[order].OrderBoggiItems[item].GroupDescription){
 															if ($scope.parseDate(response.data.Orders[order].DataOrdine)>=$scope.OrderDate){
@@ -49,6 +49,10 @@ angular.module('boggiApp')
 													$scope.OrderDate=$scope.parseDate(response.data.Orders[order].DataOrdine);
 													$scope.infos.push({date:$scope.OrderDate, name:response.data.Orders[order].OrderBoggiItems[item].GroupDescription, size: response.data.Orders[order].OrderBoggiItems[item].Taglia});
 												}
+											}
+											else {
+												$scope.gift=0;
+											}
 										}
                 }
 								console.log($scope.infos);
