05ae4baa7342f5d7105503347c49d232373afcf5
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-14 18:57:30 +0200

    changes in index

--- public/js/controller/homeController.js
+++ public/js/controller/homeController.js
@@ -1,11 +1,7 @@
 angular.module('boggiApp')
 	.controller('homeController', function($scope){
 
-		$scope.check=function(){
-			if($scope.email==""){
-				alert('error');
-				 
-			}
 
-		};
+
+		
 	});
