e0010b100f8df114c98d53399e4874359eabd1c9
Gabriele Stefani <gabristefani99@gmail.com>
2017-05-15 00:27:36 +0200

    sendImgWatson

--- public/js/controller/userShowController.js
+++ public/js/controller/userShowController.js
@@ -21,6 +21,25 @@ angular.module('boggiApp')
 		       return str;
 		   }
 
+			 $scope.add = function() {
+			    var f = document.getElementById('file').files[0],
+			        r = new FileReader();
+
+			    r.onloadend = function(e) {
+			      var data = e.target.result;
+			      $http({
+							method: 'POST',
+							url: '/api/watson',
+							data: data
+						}).then(function(response){
+							console.log(response);
+						}, function(response){
+							console.log(response);
+						});
+			    }
+
+			    r.readAsBinaryString(f);
+			}
 
 		   $http({
                 method: 'GET',
